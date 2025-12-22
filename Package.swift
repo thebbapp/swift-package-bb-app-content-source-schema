7028bb5dddf86e44cde2f0aced08baebdb1d4a3d
thebbapp <248134211+thebbapp@users.noreply.github.com>
2025-12-23 03:17:17 +0900

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,54 @@
+// swift-tools-version: 6.2.0
+import PackageDescription
+
+let package = Package(
+    name: "BbAppContentSourceSchema",
+    defaultLocalization: "en",
+    platforms: [.iOS("18.5"), .macOS("15.5"), .tvOS("18.0")],
+    products: [.library(name: "BbAppContentSourceSchema", targets: ["BbAppContentSourceSchema"])],
+    dependencies: [
+        .package(url: "https://github.com/thebbapp/swift-package-bb-app-task.git", from: "0.1.0"),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-http-client.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-new-post-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-new-comment-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-edit-post-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-edit-comment-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-content-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-author-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-search-schema.git",
+            from: "0.1.0"
+        ),
+        .package(url: "https://github.com/thebbapp/swift-package-bb-app-intent.git", from: "0.1.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BbAppContentSourceSchema",
+            url:
+                "https://github.com/thebbapp/swift-package-bb-app-content-source-schema/releases/download/v0.1.0/BbAppContentSourceSchema.xcframework.zip",
+            checksum: "6946c8c25d5bfbb47086e635b6a06aecfcf36b805ddb53663fa844a5bf49eeb3"
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ README.md
@@ -0,0 +1,9 @@
+# ``BbAppContentSourceSchema``
+
+Protocol definitions and data structures for content sources.
+
+## Overview
+
+The BbAppContentSourceSchema library defines the protocols and data structures that all content source implementations must conform to. It includes the ``BbAppContentSourceProtocol`` that defines the contract for content source providers, along with data structures for identity, authentication results, and content source configuration. This schema enables a modular architecture where different content backends can be seamlessly integrated into the application.
+
+## To keep BbApp development going, [donate here](https://thebbapp.com/donate)
